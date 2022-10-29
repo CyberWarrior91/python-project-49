434423fbd08f564716b3045fdd6a3ea9ec3401e8
Vladislav25 <vladkhleborodov@gmail.com>
2022-10-29 11:26:07 +0300

    edit second game linter errors

--- brain_games/scripts/games/brain_calc.py
+++ brain_games/scripts/games/brain_calc.py
@@ -12,31 +12,34 @@ def main():
         num_1 = random.randint(1, 99)
         num_2 = random.randint(1, 99)
         expression = f'{num_1} {action} {num_2}'
-        print(f"Question:  {expression}") 
+        print(f"Question:  {expression}")
         answer = prompt.string("Your answer: ")
         if action == "+" and answer == f"{num_1 + num_2}":
             print('Correct!')
             n += 1
         elif action == "+" and answer != f"{num_1 + num_2}":
-            print(f'"{answer}" is wrong answer ;(. Correct answer was "{num_1 + num_2}".')
+            print(f'"{answer}" is wrong answer ;(.\
+             Correct answer was "{num_1 + num_2}".')
             break
         elif action == '-' and answer == f"{num_1 - num_2}":
             print('Correct!')
             n += 1
         elif action == "-" and answer != f"{num_1 - num_2}":
-            print(f'"{answer}" is wrong answer ;(. Correct answer was "{num_1 - num_2}".')
+            print(f'"{answer}" is wrong answer ;(.\
+             Correct answer was "{num_1 - num_2}".')
             break
         elif action == '*' and answer == f"{num_1 * num_2}":
             print('Correct!')
             n += 1
-        else: 
-            print(f'"{answer}" is wrong answer ;(. Correct answer was "{num_1 * num_2}".')
+        else:
+            print(f'"{answer}" is wrong answer ;(.\
+             Correct answer was "{num_1 * num_2}".')
             break
     if n == 3:
         print(f'Congratulations, {name}!')
     else:
         print(f"Let's try again, {name}!")
 
-        
+
 if __name__ == '__main__':
     main()
